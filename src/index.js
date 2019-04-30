aafbe8eca6f3e68ff505d1f8125b4db8681add5e
Sam <sam@iMac-de-samia.local>
2019-04-30 17:38:12 +0100

    add react app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Form from "./Form";
+import ReactSearchBox from 'react-search-box';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: []
+    };
+
+    this.addPerson = this.addPerson.bind(this);
+    this.deletePerson = this.deletePerson.bind(this);
+  }
+
+  addPerson(name, email) {
+    this.setState(prevState => ({
+      people: [...prevState.people, { name, email }]
+    }));
+  }
+
+  componentDidMount() {
+    this.getPeople();
+  }
+
+  getPeople() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(response => this.setState({ people: response }))
+      .catch(error => console.log(error));
+  }
+
+  deletePerson(email) {
+    return () => {
+      this.setState(prevState => ({
+        people: prevState.people.filter(person => person.email !== email)
+      }));
+    };
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div className="App"><ReactSearchBox
+      placeholder="Placeholder"
+      value="Leanne"
+      data={this.data}
+      callback={record => console.log(record)}
+    />
+
+        <Form addPerson={this.addPerson} />
+        <table>
+          <thead>
+            <tr>
+              <th>LP</th>
+              <th>USER</th>
+              <th>EMAIL</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.people.map((person, index) => {
+              return (
+                <tr key={person.email}>
+                  <th>{index + 1}</th>
+                  <td>{person.name}</td>
+                  <td>{person.email}</td>
+                  <td>
+                    <button onClick={this.deletePerson(person.email)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
